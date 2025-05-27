a967fa600c04bcced84ca286d89676e105363647
darkVador221 <vador2899@gmail.com>
2025-05-27 20:44:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const bodyParser = require("body-parser");
-
+const __path = process.cwd();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8000;
 
-require('events').EventEmitter.defaultMaxListeners = 500;
-
-const server = require('./qr');
-const code = require('./pair');
-
-// Middleware
+// Middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-app.use('/server', server);
-app.use('/code', code);
-app.use('/pair', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pair.html'));
-});
-app.use('/qr', (req, res) => {
-  res.sendFile(path.join(__dirname, 'qr.html'));
-});
-app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'main.html'));
-});
+app.use('/server', require('./qr'));
+app.use('/code', require('./pair'));
+
+// Serve static files
+app.get('/pair', (req, res) => res.sendFile(__path + '/pair.html'));
+app.get('/qr', (req, res) => res.sendFile(__path + '/qr.html'));
+app.get('/', (req, res) => res.sendFile(__path + '/main.html'));
 
 app.listen(PORT, () => {
-  console.log(`\nDon't Forget To Give Star GAMER-XMD\n\nServer running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on port: ${PORT}`);
 });
 
 module.exports = app;
