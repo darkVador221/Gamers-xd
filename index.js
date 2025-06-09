087631055b3b70aa4fd10413c7145c430b6f26d1
darkVador221 <vador2899@gmail.com>
2025-06-09 17:44:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forget To Give Star DAMON-MD
+Don't Forgot To Give Star GAMER-XMD
 
  Server running on http://localhost:` + PORT)
 })
