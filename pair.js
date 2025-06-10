acc58d29fe63b70a351178344ff50b1980ac0778
INCONNU BOY <inconnuboy39@gmail.com>
2025-06-10 16:08:59 -0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,7 +75,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "KANGO~" + string_session;
+                        let md = "GAMER~XMD~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `*🎮Hello there GAMER-XMD User! 👋🏻* 
 
