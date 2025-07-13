f87a97eef831960202f03ad19098e39a84c10ccc
darkVador221 <vador2899@gmail.com>
2025-07-13 04:32:32 +0000

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,20 +75,20 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "GAMER-XMD~" + string_session;
+                        let md = "GAMER~XMD~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `*▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
 ╔═══✪〘 🎮 𝗕𝗢𝗧 𝗔𝗖𝗧𝗜𝗩𝗔𝗧𝗘𝗗 〙✪══⊷❍
-║👾 𝗚𝗥𝗘𝗘𝗧𝗜𝗡𝗚𝗦 » ${m.pushName}
-║🔐 𝗦𝗘𝗖𝗨𝗥𝗜𝗧𝗬 𝗔𝗟𝗘𝗥𝗧 » 𝗡𝗲𝘃𝗲𝗿 𝘀𝗵𝗮𝗿𝗲 𝘆𝗼𝘂𝗿 𝘀𝗲𝘀𝘀𝗶𝗼𝗻 𝗜𝗗!
-║💻 𝗕𝗢𝗧 » 𝗚𝗔𝗠𝗘𝗥-𝗫𝗠𝗗
+║👾 𝗚𝗥𝗘𝗘𝗧𝗜𝗡𝗚𝗦 : ${m.pushName}
+║🔐 𝗦𝗘𝗖𝗨𝗥𝗜𝗧𝗬 𝗔𝗟𝗘𝗥𝗧 : 𝗡𝗲𝘃𝗲𝗿 𝘀𝗵𝗮𝗿𝗲 𝘆𝗼𝘂𝗿 𝘀𝗲𝘀𝘀𝗶𝗼𝗻 𝗜𝗗!
+║💻 𝗕𝗢𝗧 : 𝗚𝗔𝗠𝗘𝗥-𝗫𝗠𝗗
 ╚══════════════════⊷❍
 ▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
 
 ╔═══✪〘 📡 𝗖𝗢𝗡𝗡𝗘𝗖𝗧 〙✪══⊷❍
-║📢 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 » 
+║📢 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 : 
 ║https://whatsapp.com/channel/0029VbAF9iTJUM2aPl9plJ2U
-║💾 𝗙𝗼𝗿𝗸 𝗥𝗲𝗽𝗼𝘀𝗶𝘁𝗼𝗿𝘆 » 
+║💾 𝗙𝗼𝗿𝗸 𝗥𝗲𝗽𝗼𝘀𝗶𝘁𝗼𝗿𝘆 :
 ║https://github.com/darkVador221/Inco_dark
 ╚══════════════════⊷❍
 ▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
@@ -99,7 +99,7 @@ contextInfo: {
 externalAdReply: {
 title: "DARK-GAMER",
 thumbnailUrl: "https://files.catbox.moe/zzne7x.jpeg",
-sourceUrl: "https://whatsapp.com/channel/0029VbAF9iTJUM2aPl9plJ2U",
+sourceUrl: "https://whatsapp.com/channel/0029VbAxzfJFcow0o5qexb0O",
 mediaType: 1,
 renderLargerThumbnail: true
 }  
@@ -108,14 +108,20 @@ renderLargerThumbnail: true
 {quoted:code })
                     } catch (e) {
                             let ddd = sock.sendMessage(sock.user.id, { text: e });
-                            let desc = `*Don't Share with anyone this code use for deploy KANGO-XMD*\n\n ◦ *Github:* https://github.com/darkVador221/Inco_dark`;
+                            let desc = `*❗ Session Generated But Error Occurred!*
+
+🛠️ *Don't share this with anyone.*
+
+🔗 *GitHub:* https://github.com/darkVador221/Inco_dark
+
+⚠️ *Try again if needed.*`;
                             await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "DARK-GAMER",
+title: "⚠️ GAMER-XMD ERROR",
 thumbnailUrl: "https://files.catbox.moe/zzne7x.jpeg",
-sourceUrl: "https://whatsapp.com/channel/0029VbAF9iTJUM2aPl9plJ2U",
+sourceUrl: "https://whatsapp.com/channel/0029VbAxzfJFcow0o5qexb0O",
 mediaType: 2,
 renderLargerThumbnail: true,
 showAdAttribution: true
